634431a323b78ed5463d3c8b97bb42b8a76a0b00
Chris Smith <chris@chameth.com>
2018-12-03 17:01:46 +0000

    Performance improvements

--- 03.py
+++ 03.py
@@ -2,18 +2,29 @@ import re
 from collections import defaultdict
 
 with open('data/03.txt', 'r') as file:
-    cells = defaultdict(list)
+    cells = defaultdict(lambda: 0)
     claims = map(lambda l: map(int, re.findall(r'\d+', l)), file.readlines())
     ids = set()
+    overlaps = 0
 
     for (cid, x, y, width, height) in claims:
-        ids.add(cid)
+        collision = False
         for i in range(width):
             for j in range(height):
-                cells[(x + i, y + j)].append(cid)
+                previous = cells[(x + i, y + j)]
+                if previous == 0:
+                    cells[(x + i, y + j)] = cid
+                elif previous == -1:
+                    collision = True
+                else:
+                    collision = True
+                    overlaps += 1
+                    if previous in ids:
+                        ids.remove(previous)
+                    cells[(x + i, y + j)] = -1
 
-    overlaps = [c for c in cells.values() if len(c) > 1]
-    print(len(overlaps))
+        if not collision:
+            ids.add(cid)
 
-    dupes = set(cid for sublist in overlaps for cid in sublist)
-    print(list(ids - dupes)[0])
+    print(overlaps)
+    print(list(ids)[0])
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE=csmith/aoc-2018-01
+IMAGE=csmith/aoc-2018-02
 
 docker image inspect $IMAGE >/dev/null 2>&1
 if [ $? -ne 0 ]
