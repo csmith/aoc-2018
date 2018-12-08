faa0f089019447b102dfb16e3f74e8c8cd2e4efa
Chris Smith <chris@chameth.com>
2018-12-08 23:23:26 +0000

    Tidy up day 8
    Reverse the input so we can just pop entries off the end as we use
    them, instead of tracking offsets horribly.

--- day08.nim
+++ day08.nim
@@ -1,32 +1,30 @@
-import math, sequtils, strutils
+import algorithm, math, sequtils, strutils
 
-func readNode(input: seq[string], start: int): tuple[value: int, metasum: int, offset: int] =
+func readNode(input: var seq[string]): tuple[value: int, metasum: int] =
     let
-        child_count = input[start + 0].parseInt
-        meta_count = input[start + 1].parseInt
+        child_count = input.pop.parseInt
+        meta_count = input.pop.parseInt
     var
         value = 0
         metasum = 0
-        offset = start + 2
         child_values: seq[int]
     
     for i in 0 .. child_count - 1:
-        var res = readNode(input, offset)
+        var res = readNode(input)
         child_values.add(res.value)
-        offset = res.offset
         metasum += res.metasum
 
-    for m in input[offset .. offset + meta_count - 1]:
-        var v = m.parseInt
+    for m in 0..meta_count-1:
+        var v = input.pop.parseInt
         if v > 0 and v - 1 < child_count:
             value += child_values[v - 1]
         metasum += v
-    offset += meta_count
     
     if child_count == 0:
         value = metasum
-    (value, metasum, offset)
+    (value, metasum)
 
-let root = readNode(readFile("data/08.txt").strip.split(" "), 0)
+var input = readFile("data/08.txt").strip.split(" ").reversed
+let root = input.readNode
 echo root.metasum
 echo root.value
