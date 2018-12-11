6ce0442f348500d0be8d9b9127b33e0e7a5f27e7
Chris Smith <chris@chameth.com>
2018-12-11 23:40:58 +0000

    Optimise day 10

--- day10.nim
+++ day10.nim
@@ -1,20 +1,35 @@
 import math, sequtils, strscans, strutils
 
+# Expected height of our letters
+const LetterHeight = 10
+
 type
     Point = ref object
-        x, y, dx, dy: int
+        sx, sy, x, y, dx, dy: int
 
-var points: seq[Point]
+var
+    points: seq[Point]
+    miny = int.high
+    maxy = int.low
+    mindy, maxdy = 0
 
 for line in readFile("data/10.txt").strip.splitlines:
     var point = new(Point)
-    if not line.scanf("position=<$s$i,$s$i> velocity=<$s$i,$s$i>", point.x, point.y, point.dx, point.dy):
+    if not line.scanf("position=<$s$i,$s$i> velocity=<$s$i,$s$i>", point.sx, point.sy, point.dx, point.dy):
         raise newException(Defect,  "Invalid input line: " & line)
+    if point.sy < miny:
+        miny = point.sy
+        mindy = point.dy
+    if point.sy > maxy:
+        maxy = point.sy
+        maxdy = point.dy
     points.add(point)
 
-var
-    t = 0
-    last_distance = int.high
+# Compute the first time at which the most outlying stars will be within
+# letter-height of each other. It may take a few more iterations after
+# this for them to get into their final place.
+var t = (maxy - miny - LetterHeight) div (mindy - maxdy)
+
 while true:
     var
         minx = int.high
@@ -22,20 +37,19 @@ while true:
         miny = int.high
         maxy = int.low
     for point in points:
-        point.x += point.dx
-        point.y += point.dy
+        point.x = point.sx + point.dx * t
+        point.y = point.sy + point.dy * t
         minx = min(minx, point.x)
         maxx = max(maxx, point.x)
         miny = min(miny, point.y)
         maxy = max(maxy, point.y)
 
-    var distance = (maxx - minx) * (maxy - miny)
-    if distance > last_distance:
+    if maxy - miny == LetterHeight - 1:
         for y in miny..maxy:
             var grid = newSeq[bool](1 + maxx - minx)
             for point in points:
-                if point.y - point.dy == y:
-                    grid[point.x - point.dx - minx] = true
+                if point.y == y:
+                    grid[point.x - minx] = true
             echo grid.map(proc(cell: bool): string =
                 if cell:
                     "█"
@@ -43,6 +57,5 @@ while true:
                     " ").join
         echo t
         break
-    else:
-        last_distance = distance
-        t.inc
+    
+    t.inc
