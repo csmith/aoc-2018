d137fdb031a8a0d337aeba4f691076b01ef5d5bd
Chris Smith <chris@chameth.com>
2018-12-12 00:36:45 +0000

    Minor day 11 optimisations
    Calculate the power inline so we don't have to repeat the rack ID
    etc every time; give in and use 1-based arrays instead of mapping
    the 1-base back to 0-base.

--- day11.nim
+++ day11.nim
@@ -1,21 +1,22 @@
 import math, sequtils, strscans, strutils
 
-func power(serial, x, y: int): int =
-    let rackId = x + 10
-    (rackId * y + serial) * rackId div 100 mod 10 - 5
-
 var
     serial = readFile("data/11.txt").strip.parseInt
-    prefixSum: array[-1..300, array[-1..300, int]]
+    prefixSum: array[301, array[301, int]]
 
-for x in 0..299:
-    for y in 0..299:
+for x in 1..300:
+    let serialByRack = (x + 10) * serial
+    let rackSquared = (x + 10) * (x + 10)
+    var tally = serialByRack
+    for y in 1..300:
+        tally.inc(rackSquared)
+        let power = tally div 100 mod 10 - 5
         # Construct a prefix sum of the grid: prefixSum[x][y] holds the sum of
         # all cells in the rectangle from (0,0) to (x,y). We calculate this
         # by adding the current cell to the rectangle above and the rectangle
         # to the left. This results in double-counting the rect between (0,0)
         # and (x-1,y-1), so we subtract that from the result.
-        prefixSum[x][y] = power(serial, x+1, y+1) + prefixSum[x][y-1] +
+        prefixSum[x][y] = power + prefixSum[x][y-1] +
                             prefixSum[x-1][y] - prefixSum[x-1][y-1]
 
 var
@@ -26,8 +27,8 @@ var
 
 for size in 1..300:
     let s1 = size - 1
-    for x in 0..300 - size:
-        for y in 0..300 - size:
+    for x in 1..300 - size:
+        for y in 1..300 - size:
             # To use the prefix sum to find the value of a rect, we take the
             # value for the lower right corner and subtract the rects to the
             # left and the top. Like when constructing the prefix sum this
@@ -37,10 +38,10 @@ for size in 1..300:
                         prefixSum[x+s1][y-1] + prefixSum[x-1][y-1]
             if sum > best:
                 best = sum
-                bestPos = $(x + 1) & "," & $(y + 1) & "," & $size
+                bestPos = $x & "," & $y & "," & $size
             if size == 3 and sum > bestThree:
                 bestThree = sum
-                bestThreePos = $(x + 1) & "," & $(y + 1)
+                bestThreePos = $x & "," & $y
 
 echo bestThreePos
 echo bestPos
