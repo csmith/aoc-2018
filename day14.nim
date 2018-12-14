f1e282473479e276d4cdda01134f2cdab30e2888
Chris Smith <chris@chameth.com>
2018-12-14 10:29:21 +0000

    Minor optimisations

--- day14.nim
+++ day14.nim
@@ -2,12 +2,13 @@ import sequtils, strutils
 
 let
     input = readFile("data/14.txt").strip
-    inputSeq = input.map do (c: char) -> int: cast[int](c) - 48
+    inputSeq = input.map do (c: char) -> int8: cast[int8](c.int - '0'.int)
+    inputHead = inputSeq[0..inputSeq.len-2]
     lastDigit = inputSeq[inputSeq.len - 1]
     inputInt = input.parseInt
 
 var
-    scores: array[50_000_000, int]  # Arbitrary large preallocation
+    scores: array[50_000_000, int8]  # Arbitrary large preallocation
     size = 2
     elf1 = 0
     elf2 = 1
@@ -20,18 +21,17 @@ while size < inputInt + 10 or part2 == -1:
     var score = scores[elf1] + scores[elf2]
     
     if score >= 10:
-        let newScore = score div 10
-        scores[size] = newScore
+        scores[size] = 1
         size.inc
 
-        if newScore == lastDigit and size > input.len and scores[size-input.len..size-1] == inputSeq:
+        if 1 == lastDigit and size > input.len and scores[size-input.len..size-2] == inputHead:
             part2 = size - input.len
 
-    let newScore = score mod 10
+    let newScore = cast[int8](score mod 10)
     scores[size] = newScore
     size.inc
 
-    if newScore == lastDigit and size > input.len and scores[size-input.len..size-1] == inputSeq:
+    if newScore == lastDigit and size > input.len and scores[size-input.len..size-2] == inputHead:
         part2 = size - input.len
 
     elf1 = (elf1 + scores[elf1] + 1) mod size
