877a6cca07e790b67b121c3b3fe27151218f9788
Chris Smith <chris@chameth.com>
2018-12-16 19:26:37 +0000

    Tidy and optimise day 14 a little

--- day14.nim
+++ day14.nim
@@ -3,9 +3,8 @@ import sequtils, strutils
 let
     input = readFile("data/14.txt").strip
     inputSeq = input.map do (c: char) -> int8: cast[int8](c.int - '0'.int)
-    inputHead = inputSeq[0..inputSeq.len-2]
-    lastDigit = inputSeq[inputSeq.len - 1]
     inputInt = input.parseInt
+    inputLen = input.len
 
 var
     scores: array[50_000_000, int8]  # Arbitrary large preallocation
@@ -13,6 +12,16 @@ var
     elf1 = 0
     elf2 = 1
     part2 = -1
+
+proc check(inputSeq: seq[int8], val: int, part2: var int) {.inline.} =
+    var found {.global.} = 0
+    if val == inputSeq[found]:
+        found.inc
+        if found == inputLen:
+            part2 = size - inputLen
+            found = 0
+    else:
+        found = 0
 
 scores[0] = 3
 scores[1] = 7
@@ -23,16 +32,13 @@ while size < inputInt + 10 or part2 == -1:
     if score >= 10:
         scores[size] = 1
         size.inc
-
-        if 1 == lastDigit and size > input.len and scores[size-input.len..size-2] == inputHead:
-            part2 = size - input.len
+        inputSeq.check(1, part2)
 
     let newScore = cast[int8](score mod 10)
     scores[size] = newScore
     size.inc
 
-    if newScore == lastDigit and size > input.len and scores[size-input.len..size-2] == inputHead:
-        part2 = size - input.len
+    inputSeq.check(newScore, part2)
 
     elf1 = (elf1 + scores[elf1] + 1) mod size
     elf2 = (elf2 + scores[elf2] + 1) mod size
