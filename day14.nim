c1820b60c577d23f401a6a8d8ab272b76fa5c3fc
Chris Smith <chris@chameth.com>
2018-12-16 19:46:31 +0000

    Just use a sequence not an array
    It's about 30% slower, but a lot nicer.

--- day14.nim
+++ day14.nim
@@ -7,8 +7,7 @@ let
     inputLen = input.len
 
 var
-    scores: array[50_000_000, int8]  # Arbitrary large preallocation
-    size = 2
+    scores = @[3'i8, 7'i8]
     elf1 = 0
     elf2 = 1
     part2 = -1
@@ -18,30 +17,25 @@ proc check(inputSeq: seq[int8], val: int, part2: var int) {.inline.} =
     if val == inputSeq[found]:
         found.inc
         if found == inputLen:
-            part2 = size - inputLen
+            part2 = scores.len - inputLen
             found = 0
     else:
         found = 0
 
-scores[0] = 3
-scores[1] = 7
-
-while size < inputInt + 10 or part2 == -1:
+while scores.len < inputInt + 10 or part2 == -1:
     var score = scores[elf1] + scores[elf2]
     
     if score >= 10:
-        scores[size] = 1
-        size.inc
+        scores.add(1)
         inputSeq.check(1, part2)
 
     let newScore = cast[int8](score mod 10)
-    scores[size] = newScore
-    size.inc
+    scores.add(newScore)
 
     inputSeq.check(newScore, part2)
 
-    elf1 = (elf1 + scores[elf1] + 1) mod size
-    elf2 = (elf2 + scores[elf2] + 1) mod size
+    elf1 = (elf1 + scores[elf1] + 1) mod scores.len
+    elf2 = (elf2 + scores[elf2] + 1) mod scores.len
 
 echo scores[inputInt..inputInt+9].join
 echo part2
